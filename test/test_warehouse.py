1089e6ac322146278e6f7f169b6e3297c8a738b4
Alline <allinem196@gmail.com>
2024-03-18 14:29:07 +0000

    test if catalogue add only unique entries

--- test/test_warehouse.py
+++ test/test_warehouse.py
@@ -9,6 +9,7 @@ eletric_amp = Product(4, "Eletric Amp", 50)
 first_entry = Entry(guitar_gibson, 40)
 second_entry = Entry(guitar_fender, 20)
 third_entry = Entry(accoustic_guitar, 15)
+fourth_entry = Entry(eletric_amp, 15)
 def test_if_entry_returns_product_description():
     assert first_entry.product.description == "Gibson Les Paul"
 
@@ -18,3 +19,11 @@ def test_if_entry_returns_stock():
 def test_if_catalogue_is_initialised_empty():
     warehouse = Warehouse()
     assert len(warehouse.catalogue) == 0
+
+def test_if_catalogue_has_unique_entry():
+    warehouse = Warehouse()
+    warehouse.add_entry(first_entry)
+    warehouse.add_entry(second_entry)
+    warehouse.add_entry(third_entry)
+    warehouse.add_entry(third_entry)
+    assert len(warehouse.catalogue) == 3
