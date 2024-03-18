82a88028d0535dfd096a3261985ccf263b169253
Alline <allinem196@gmail.com>
2024-03-18 13:57:18 +0000

    test address city

--- test/test_address.py
+++ test/test_address.py
@@ -4,3 +4,7 @@ from src.countries import Country
 def test_if_country_in_list_of_country():
     anna_address = Address("50", "Vrijdagmarkt", "Gent", "900", Country.BELGIUM)
     assert anna_address.country in Country
+
+def test_address_city():
+    john_address = Address("234", "Viale Emilio Alemagna", "Milan", "201221", Country.ITALY)
+    assert john_address.city == "Milan"
