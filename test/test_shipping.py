1301b221a6c8b5c6c99d8a29997ba9ba4c8885ab
Alline <allinem196@gmail.com>
2024-03-18 16:19:57 +0000

    parametrize shipping tests

--- test/test_shipping.py
+++ test/test_shipping.py
@@ -1,7 +1,15 @@
 from src.countries import Country
 from src.shipping import calculate_shipping
+import pytest
 
-def test_when_uk_region_order_higher_than_hundred():
-    uk = Country.UNITED_KINGDOM.value
-    shipping = calculate_shipping(country=uk, order_total=101)
-    assert shipping == 0.0
+@pytest.mark.parametrize("country, order_total, expected_value",[
+    (Country.UNITED_KINGDOM.value, 101, 0.0),
+    (Country.UNITED_KINGDOM.value, 90, 4.99),
+    (Country.BELGIUM.value, 150, 4.99),
+    (Country.NETHERLANDS.value, 50, 8.99),
+    (Country.UKRAINE.value, 100, 9.99),
+    (Country.UKRAINE.value, 50, 9.99)
+])
+def test_when_uk_region_order_higher_than_hundred(country, order_total, expected_value):
+    shipping = calculate_shipping(country=country, order_total=order_total)
+    assert shipping == expected_value
