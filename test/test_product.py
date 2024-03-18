eea62d51541db911ce6b1c89aa9d1e15b9549433
Alline <allinem196@gmail.com>
2024-03-18 13:42:28 +0000

    add product id test

--- test/test_product.py
+++ test/test_product.py
@@ -7,3 +7,7 @@ def test_product_description():
 def test_product_price():
     bass = Product(2, "bass", 150)
     assert bass.price == 150
+
+def test_product_id():
+    accoustic_guitar = Product(3, "Accoustic Guitar", 100)
+    assert accoustic_guitar.id == 3
