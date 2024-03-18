0b87d432eabe0266a4da81d4bb7380b630b5d6cc
Alline <allinem196@gmail.com>
2024-03-18 14:03:09 +0000

    parametrize address country tests

--- test/test_address.py
+++ test/test_address.py
@@ -1,10 +1,16 @@
 from src.address import Address
 from src.countries import Country
-
+import pytest
 def test_if_country_in_list_of_country():
     anna_address = Address("50", "Vrijdagmarkt", "Gent", "900", Country.BELGIUM)
     assert anna_address.country in Country
 
-def test_address_city():
-    john_address = Address("234", "Viale Emilio Alemagna", "Milan", "201221", Country.ITALY)
-    assert john_address.city == "Milan"
+@pytest.mark.parametrize("house, street, city, postcode, country, expected_country", [
+    ("50", "Vrijdagmarkt", "Gent", "900", Country.BELGIUM,Country.BELGIUM ),
+    ("11", "Holywell Close", "London", "SE3", Country.UNITED_KINGDOM, Country.UNITED_KINGDOM),
+    ("123", "Vilae Emilio Alemganha", "Milan", "12414", Country.ITALY, Country.ITALY),
+      
+])
+def test_address_city(house, street, city, postcode, country, expected_country):
+    client_address = Address(house, street,city, postcode, country)
+    assert client_address.country == expected_country
