197b8af1758b97d5ca227523df8e2a4c1867659f
Alline <allinem196@gmail.com>
2024-03-18 14:13:52 +0000

    add entry test for stock

--- test/test_warehouse.py
+++ test/test_warehouse.py
@@ -9,3 +9,7 @@ eletric_amp = Product(4, "Eletric Amp", 50)
 def test_if_entry_returns_product_description():
     first_entry = Entry(guitar_gibson, 40)
     assert first_entry.product.description == "Gibson Les Paul"
+
+def test_if_entry_returns_stock():
+    second_entry = Entry(guitar_fender, 20)
+    assert second_entry.stock == 20
