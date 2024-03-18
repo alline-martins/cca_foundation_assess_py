f000adafe1ce420e74c986b12d08c84dd3a2ba3c
Alline <allinem196@gmail.com>
2024-03-18 13:40:01 +0000

    test product price

--- test/test_product.py
+++ test/test_product.py
@@ -3,3 +3,7 @@ from src.product import Product
 def test_product_description():
     eletric_guitar = Product(1, "Gibson Les Paul", 229)
     assert eletric_guitar.description == "Gibson Les Paul"
+
+def test_product_price():
+    bass = Product(2, "bass", 150)
+    assert bass.price == 150
